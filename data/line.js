45b995cfee1d0f363621c525b31cd7fac313523c
MohakKala07 <84536833+MohakKala07@users.noreply.github.com>
2022-07-30 03:00:33 +0530

    Repository Updated

---
+++ data/line.js
@@ -0,0 +1,94 @@
+var lineJ={
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [
+              73.1689453125,
+              22.228090416784486
+            ],
+            [
+              73.63037109375,
+              18.687878686034182
+            ],
+            [
+              74.44335937499999,
+              15.749962572748768
+            ]
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [
+              88.3740234375,
+              22.553147478403194
+            ],
+            [
+              80.44189453125,
+              26.352497858154024
+            ]
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [
+              77.1240234375,
+              28.362401735238237
+            ],
+            [
+              75.82763671875,
+              27.0982539061379
+            ]
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [
+              80.13427734374999,
+              13.21855594917547
+            ],
+            [
+              73.98193359375,
+              18.729501999072138
+            ]
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {},
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [
+              87.71484375,
+              22.28909641872304
+            ],
+            [
+              83.07861328125,
+              17.895114303749143
+            ]
+          ]
+        }
+      }
+    ]
+  }
